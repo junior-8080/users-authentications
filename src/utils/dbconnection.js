4acd953fe893f54e8946f0538dbda99402e14f56
Abdul Mukhsin <abdul@esoko.com>
2022-08-15 22:19:21 +0000

    interagte heroku-postgres

--- src/utils/dbconnection.js
+++ src/utils/dbconnection.js
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize";
 import logger from "../logs/logger.js";
 
-export const sequelize = new Sequelize('postgres://postgres:qwerty123@localhost:5432/users-authentication');
+export const sequelize = new Sequelize(`${process.env.DB_DIALECT}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
 
 export const dbInit = () => {
     try {
--- src/utils/thirdparties.js
+++ src/utils/thirdparties.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-let accountBaseUrl = `http://localhost:3001/v1/auth/api-key/`
+let accountBaseUrl = `${process.env.ACCOUNT_API_BASE_URL}/api-key/`
 export const getAccountDeatils = async (apiKey) => {
   try {
     const response = await axios.get(`${accountBaseUrl}${apiKey}`);
