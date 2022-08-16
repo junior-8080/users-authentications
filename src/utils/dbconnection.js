04afee013252f0c3ea8a70b0cc621800a317dc4c
Abdul Mukhsin <abdul@esoko.com>
2022-08-16 07:47:35 +0000

    chnage sequelize connection to ssl

--- src/utils/dbconnection.js
+++ src/utils/dbconnection.js
@@ -1,7 +1,9 @@
 import {Sequelize} from "sequelize";
 import logger from "../logs/logger.js";
 
-export const sequelize = new Sequelize(`${process.env.DB_DIALECT}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+export const sequelize = new Sequelize(`${process.env.DB_DIALECT}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,{
+    ssl:true
+});
 
 export const dbInit = () => {
     try {
